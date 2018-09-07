7681d20e91b38cf8b96514c51d36d89fc7e1b290
PengInGitHub <felixdeutschland@hotmail.com>
2018-09-07 22:03:23 +0200

    21.MergeTwoSortedLists

---
+++ 21.MergeTwoSortedLists/Solution.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        #use pointer
+        head = ListNode(0)
+        tail = head
+        while l1 is not None and l2 is not None:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        return head.next
+    
