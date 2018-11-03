df9dc214d47e3ad22773a62d1e82d9119689dee1
PengInGitHub <felixdeutschland@hotmail.com>
2018-11-03 08:57:11 +0100

    add new 929.UniqueEmailAddresses

---
+++ 929.UniqueEmailAddresses/s.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        res = set()
+        for email in emails:
+            local, domain = (email.split('@')[0].split('+')[0]).replace('.',''), email.split('@')[1]
+            res.add(local+domain)
+        return len(res)
+            
